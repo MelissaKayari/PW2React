59585ee0414c6b2ed33dbee091ca56091d0475fe
Melissa Kayari de Lima Rocha <kayarimelissa@gmail.com>
2026-09-09 22:42:07 -0300

    Add files via upload

---
+++ src/app.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App(){
+    return (
+        <div classname="bg-light min-vh-100- py-4">
+            <div classname="container">
+                <header classname="row mb-4">
+                    <div classname="col test-center">
+                        <h1 classname="display-5 fw-bold text-primary">
+                            Projeto React + Bootstrap Grid
+                        </h1>
+                        <p classname="read text-secundary">
+                            Exemplo de layout responsivo utilizando o sistema
+                            de colunas no Bootstrap
+                        </p>
+                    </div>
+                </header>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "start-dom/client";
+import App from "App";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.createRoot(document.getElementById('root'))-render(
+    <React.scriptMode>
+        <App/>
+    </React.scriptMode>
+
+);
